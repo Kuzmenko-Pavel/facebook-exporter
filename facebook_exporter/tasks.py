59fbb58647bd11878341682e3c3ea901ce43d71e
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-08-27 16:01:41 +0300

    change select

--- facebook_exporter/tasks.py
+++ facebook_exporter/tasks.py
@@ -23,63 +23,67 @@ tpl_xml_end = '''\n</channel>\n</rss>'''
 
 @app.task(ignore_result=True)
 def check_feed():
-        dbsession = app.conf['PYRAMID_REGISTRY']['dbsession_factory']()
-        with transaction.manager:
-                result = dbsession.execute('''SELECT
-                                              a.AdvertiseID AS AdvertiseID
-                                              FROM Advertise a
-                                              LEFT OUTER JOIN MarketByAdvertise mark ON mark.AdvertiseID = a.AdvertiseID
-                                              where MarketID is not NULL
-                                                ''')
-                for adv in result:
-                        create_feed.delay(adv[0])
+    print('START RECREATE FEED')
+    dbsession = app.conf['PYRAMID_REGISTRY']['dbsession_factory']()
+    with transaction.manager:
+            result = dbsession.execute('''SELECT
+                                          a.AdvertiseID AS AdvertiseID
+                                          FROM Advertise a
+                                          LEFT OUTER JOIN MarketByAdvertise mark ON mark.AdvertiseID = a.AdvertiseID
+                                          where MarketID is not NULL
+                                            ''')
+            for adv in result:
+                    create_feed.delay(adv[0])
+    print('STOP RECREATE FEED')
 
 
 @app.task(ignore_result=True)
 def create_feed(id):
-        count = 1000
-        q = '''
-                        SELECT TOP %s  
-                        View_Lot.LotID AS LotID,
-                        View_Lot.Title AS Title,
-                        ISNULL(View_Lot.Descript, '') AS Description,
-                        ISNULL(View_Lot.Price, '0') Price,
-                        View_Lot.ExternalURL AS UrlToMarket,
-                        View_Lot.ImgURL,
-                        RetargetingID 
-                        FROM View_Lot 
-                        INNER JOIN LotByAdvertise ON LotByAdvertise.LotID = View_Lot.LotID
-                        INNER JOIN View_Advertise ON View_Advertise.AdvertiseID = LotByAdvertise.AdvertiseID
-                        WHERE View_Advertise.AdvertiseID = '%s' AND View_Lot.ExternalURL <> '' 
-                            AND View_Lot.isTest = 1 AND View_Lot.isAdvertising = 1
-                        ''' % (count, id)
+    print('START CREATE FEED %s' % id)
+    count = 1000000
+    q = '''
+                    SELECT TOP %s  
+                    View_Lot.LotID AS LotID,
+                    View_Lot.Title AS Title,
+                    ISNULL(View_Lot.Descript, '') AS Description,
+                    ISNULL(View_Lot.Price, '0') Price,
+                    View_Lot.ExternalURL AS UrlToMarket,
+                    View_Lot.ImgURL,
+                    RetargetingID 
+                    FROM View_Lot 
+                    INNER JOIN LotByAdvertise ON LotByAdvertise.LotID = View_Lot.LotID
+                    INNER JOIN View_Advertise ON View_Advertise.AdvertiseID = LotByAdvertise.AdvertiseID
+                    WHERE View_Advertise.AdvertiseID = '%s' AND View_Lot.ExternalURL <> '' 
+                        AND View_Lot.isTest = 1 AND View_Lot.isAdvertising = 1
+                    ''' % (count, id)
 
-        dbsession = app.conf['PYRAMID_REGISTRY']['dbsession_factory']()
-        dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/xml')
-        file_path = os.path.join(dir_path, id + ".xml")
-        temp_file = file_path + '.' + str(int(time.time()))
-        with open(temp_file, 'w') as f:
-            f.write(tpl_xml_start)
-            with transaction.manager:
-                    result = dbsession.execute(q)
-                    for offer in result:
-                        try:
-                            offer_id = '%s...%s' % (offer[0], id)
-                            if offer[6]:
-                                offer_id = '%s...%s' % (offer[6], id)
-                            data = tpl_xml_offer % (
-                                offer_id,
-                                html.escape(offer[1]),
-                                html.escape(offer[2]),
-                                offer[3],
-                                redirect_link(offer[4], offer[0], id),
-                                image_link(offer[5])
-                            )
-                        except Exception as e:
-                            print(e)
-                        else:
-                            f.write(data)
-            f.write(tpl_xml_end)
-            os.fsync(f)
+    dbsession = app.conf['PYRAMID_REGISTRY']['dbsession_factory']()
+    dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/xml')
+    file_path = os.path.join(dir_path, id + ".xml")
+    temp_file = file_path + '.' + str(int(time.time()))
+    with open(temp_file, 'w') as f:
+        f.write(tpl_xml_start)
+        with transaction.manager:
+                result = dbsession.execute(q)
+                for offer in result:
+                    try:
+                        offer_id = '%s...%s' % (offer[0], id)
+                        if offer[6]:
+                            offer_id = '%s...%s' % (offer[6], id)
+                        data = tpl_xml_offer % (
+                            offer_id,
+                            html.escape(offer[1]),
+                            html.escape(offer[2]),
+                            redirect_link(offer[4], offer[0], id),
+                            image_link(offer[5]),
+                            offer[3]
+                        )
+                    except Exception as e:
+                        print(e)
+                    else:
+                        f.write(data)
+        f.write(tpl_xml_end)
+        os.fsync(f)
 
-        move(temp_file, file_path)
+    move(temp_file, file_path)
+    print('STOP CREATE FEED %s' % id)

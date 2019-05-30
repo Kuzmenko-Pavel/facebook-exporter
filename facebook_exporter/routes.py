476fc9193b237f04de9f7e0fb1ea02d2e84fc36b
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2019-05-30 13:32:48 +0300

    delete capitalize for title and description

--- facebook_exporter/routes.py
+++ facebook_exporter/routes.py
@@ -7,4 +7,5 @@ def includeme(config):
     config.add_route('campaigns', '/campaigns')
     config.add_route('check_feed', '/check_feed')
     config.add_route('export', '/export/{id:[0-9a-fA-F\-]{36}}.xml')
+    config.add_route('export_google', '/export_google/{id:[0-9a-fA-F\-]{36}}.xml')
     config.add_static_view('static', 'static', cache_max_age=0)

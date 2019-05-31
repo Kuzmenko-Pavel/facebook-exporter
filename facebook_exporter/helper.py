9230e7b0a924d0535dde45961439ef61ebe8419e
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2019-05-31 11:46:55 +0300

    google format

--- facebook_exporter/helper.py
+++ facebook_exporter/helper.py
@@ -61,10 +61,10 @@ def price(p, default=None):
 
 
 def text_normalize(text):
-    words = text.split()
-    upper_words = [word for word in words if word.isupper()]
-    if len(upper_words) >= len(words):
-        text = text.capitalize()
+    # words = text.split()
+    # upper_words = [word for word in words if word.isupper()]
+    # if len(upper_words) >= len(words):
+    #     text = text.capitalize()
     return html.escape(text)
 
 

909a41b2668276a1755b6190fe9c644c69a58972
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2019-11-16 17:03:20 +0200

    google format

--- facebook_exporter/models/ParentOffers.py
+++ facebook_exporter/models/ParentOffers.py
@@ -5,7 +5,7 @@ __author__ = 'kuzmenko-pavel'
 
 from sqlalchemy import Column, String, Integer, BigInteger, Text
 from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy_utils import force_auto_coercion, force_instant_defaults, ChoiceType, URLType, LtreeType
+from sqlalchemy_utils import force_auto_coercion, force_instant_defaults, ChoiceType, URLType, LtreeType, UUIDType
 from facebook_exporter.models.choiceTypes import CurrencyType
 
 from .custom_arrays import ArrayOfCustomType
@@ -18,8 +18,11 @@ force_instant_defaults()
 class ParentOffer(ParentBase):
     __tablename__ = 'v_facebook_offers'
     id = Column(BigInteger, primary_key=True)
+    guid = Column(UUIDType(binary=True))
     id_campaign = Column(BigInteger)
     id_account = Column(BigInteger)
+    guid_account = Column(BigInteger)
+    guid_campaign = Column(BigInteger)
     title = Column(String)
     description = Column(String)
     url = Column(URLType)

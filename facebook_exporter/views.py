122fdc5da25aac98c92a8d5cad635397303fe3d7
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2019-11-16 17:35:35 +0200

    google format

--- facebook_exporter/views.py
+++ facebook_exporter/views.py
@@ -48,97 +48,22 @@ def export(request):
         if static:
             create_feed.delay(id)
 
-    # q = '''
-    #                 SELECT TOP %s
-    #                 View_Lot.LotID AS LotID,
-    #                 View_Lot.Title AS Title,
-    #                 ISNULL(View_Lot.Descript, '') AS Description,
-    #                 ISNULL(View_Lot.Price, '0') Price,
-    #                 View_Lot.ExternalURL AS UrlToMarket,
-    #                 View_Lot.ImgURL,
-    #                 RetargetingID,
-    #                 View_Lot.Auther,
-    #                 View_Lot.DateCreate
-    #                 FROM View_Lot
-    #                 INNER JOIN LotByAdvertise ON LotByAdvertise.LotID = View_Lot.LotID
-    #                 INNER JOIN View_Advertise ON View_Advertise.AdvertiseID = LotByAdvertise.AdvertiseID
-    #                 WHERE View_Advertise.AdvertiseID = '%s' AND View_Lot.ExternalURL <> ''
-    #                     AND View_Lot.isTest = 1 AND View_Lot.isAdvertising = 1
-    #                 ''' % (count, id)
-    # result = request.dbsession.execute(q)
-
     campaign = request.dbsession.query(ParentCampaign).filter(ParentCampaign.guid == id).one_or_none()
     if campaign:
         for offer in request.dbsession.query(ParentOffer).filter(ParentOffer.id_campaign == campaign.id).all():
-            offer_id = '%s...%s' % (offer[0], offer[7])
-            if offer[6]:
-                offer_id = '%s...%s' % (offer[6], offer[7])
+            offer_id = '%s...%s' % (str(offer.guid).upper(), str(offer.guid_account).upper())
+            if offer.id_retargeting:
+                offer_id = '%s...%s' % (offer.id_retargeting, str(offer.guid_account).upper())
             offers.append({
                 'id': offer_id,
-                'title': str(offer[1]),
-                'description': str(offer[2]),
-                'price': price(offer[3]),
-                'url': redirect_link(offer[4], offer[0], id),
-                'image': image_link(offer[5]),
-                'date': offer[8]
+                'title': str(offer.title),
+                'description': str(offer.description),
+                'price': price(offer.price),
+                'url': redirect_link(offer.url, str(offer.guid).upper(), str(id).upper()),
+                'image': image_link(offer.images[0]),
             })
 
     return {'offers': offers}
-
-
-@view_config(route_name='export_google', renderer='templates/google.html', permission='view')
-def export_google(request):
-    request.response.content_type = 'application/xml'
-    offers = []
-    count = request.GET.get('count', 1000)
-    # static = True if request.GET.get('static') == 'true' else False
-    id = request.matchdict.get('id', '').upper()
-    # dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/xml')
-    # file_path = os.path.join(dir_path, id + ".xml")
-    # file_exists = os.path.isfile(file_path)
-    # if id:
-    #     if file_exists and static:
-    #         response = FileResponse(
-    #             file_path,
-    #             request=request,
-    #             content_type=request.response.content_type
-    #         )
-    #         return response
-    #     if static:
-    #         create_feed.delay(id)
-    q = '''
-                    SELECT TOP %s
-                    View_Lot.LotID AS LotID,
-                    View_Lot.Title AS Title,
-                    ISNULL(View_Lot.Descript, '') AS Description,
-                    ISNULL(View_Lot.Price, '0') Price,
-                    View_Lot.ExternalURL AS UrlToMarket,
-                    View_Lot.ImgURL,
-                    RetargetingID,
-                    View_Lot.Auther,
-                    View_Lot.DateCreate
-                    FROM View_Lot
-                    INNER JOIN LotByAdvertise ON LotByAdvertise.LotID = View_Lot.LotID
-                    INNER JOIN View_Advertise ON View_Advertise.AdvertiseID = LotByAdvertise.AdvertiseID
-                    WHERE View_Advertise.AdvertiseID = '%s' AND View_Lot.ExternalURL <> ''
-                        AND View_Lot.isTest = 1 AND View_Lot.isAdvertising = 1
-                    ''' % (count, id)
-    result = request.dbsession.execute(q)
-    for offer in result:
-        offer_id = '%s...%s' % (uuid_to_long(offer[0]), uuid_to_long(offer[7]))
-        if offer[6]:
-            offer_id = '%s...%s' % (uuid_to_long(offer[6]), uuid_to_long(offer[7]))
-        offers.append({
-            'id': offer_id,
-            'title': str(offer[1]),
-            'description': str(offer[2]),
-            'price': price(offer[3], default=1),
-            'url': redirect_link(offer[4], offer[0], id),
-            'image': image_link(offer[5]),
-            'date': offer[8]
-        })
-
-    return {'offers': offers, 'date': datetime.now()}
 
 
 @view_config(route_name='campaigns', renderer='json', permission='view')
